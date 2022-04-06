0060e382c7b2be81f58ec1ffda62363233a6bd11
Fjodor <fjodorvanhilst@hotmail.com>
2022-04-06 23:08:07 +0200

    js toegevoegd met price loggen for filteren later van prijzen met inviseble span tag. Items worden nu geadd naar 'winkelwagen'. CSS shadow toegevoegd.

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(()=>{
+
+    let itemsInCard = 0
+
+    $('#foldOutButton').on('click', function(){
+        $('.sidebar form').slideToggle();
+    })
+
+    $('main').on('click','div',function(){
+
+        //winkelwagen itemsnum ophogen 
+        itemsInCard++
+        $('#itemsInCard').text(itemsInCard)
+
+        //hieronder wordt het bedrag op gehaald van de span met de class 'cost'
+        //dat is handig voor alls we later moeten filteren (voor nu alleen console output)
+        console.log(parseFloat($(this).find('.cost').text()))
+    })
+
+})
+
