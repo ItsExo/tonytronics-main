fb88725b2977bd6ce70959cf43eb1373a974dd67
Fjodor <fjodorvanhilst@hotmail.com>
2022-04-07 09:31:03 +0200

    some js

--- script.js
+++ script.js
@@ -15,7 +15,27 @@ $(document).ready(()=>{
         //hieronder wordt het bedrag op gehaald van de span met de class 'cost'
         //dat is handig voor alls we later moeten filteren (voor nu alleen console output)
         console.log(parseFloat($(this).find('.cost').text()))
+        
     })
 
+    // $('.priceFilter input').on('change', function(){
+    //     console.log($(this).val())
+    // })
+
+    // let maxPriceRange = 10
+
+    // $('main').children('div').each(function() {
+
+    //     if(parseFloat($(this).find('.cost').text())>maxPriceRange){
+    //         $(this).hide()
+            
+    //     }
+    //     else{
+    //         $(this).show()
+    //     }
+        
+    // });
+    
+    
 })
 
